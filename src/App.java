85b5e1f39238f59d09280c981266f4aebe4ce84e
1ebenezer <eben4nkusi@gmail.com>
2023-01-26 19:31:07 +0200

    commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+//import java.util;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)  
+    {
+        Scanner in  = new Scanner(System.in);
+        System.out.println ("enter your age :");
+        int age = in.nextInt();
+        if(age>18)
+        {
+           System.out.println("you're allowed to drink");
+        }
+        else
+        {
+            System.out.println("you're underage jya kuryama");
+        }
+
+    }
+}
